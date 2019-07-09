aa4e7af0fba2e1c3265b91b3921a892fc18cab73
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-09 19:45:33 -0400

    search bar and click injs

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,13 +1,31 @@
 var topics = ["trees", "tigers", "traveling", "bananas", "texas", "succulents", "sunshine", "running", "paris", "sleep"];
 
-function createButtons () {
-    $('#buttonDisplay').empty();
-    for (var i = 0; i < topics.length; i++) {
-        var button = $('<button>');
-        button.attr('data-name', topics[i]);
-        button.addClass('topic')
-        button.text(topics[i]);
-        $('#buttonDisplay').append(button);
+$(document).ready(function() {
+    createButtons();
+    //Building buttons to loop through array
+    function createButtons () {
+        $('#buttonDisplay').empty();
+        for (var i = 0; i < topics.length; i++) {
+            var button = $('<button>');
+            button.attr('data-name', topics[i]);
+            button.addClass('topic')
+            button.addClass('btn btn-light')
+            button.text(topics[i]);
+            $('#buttonDisplay').append(button);
+        }
     }
-}
-createButtons();
+
+    //building click functionality
+    $('#addTopic').on('click', function(event) {
+        event.preventDefault();
+
+        var addedTopic = $('#inputTopic').val().trim();
+        console.log(addedTopic);
+
+        topics.push(addedTopic);
+        $('#inputTopic').val("");
+        createButtons();
+    })
+
+    //call to GIPHY api
+})
