66772932917a192719985a39b96eff51670e095d
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-09 21:04:34 -0400

    correct giphy calls, undefined images return

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,5 @@
-var topics = ["trees", "tigers", "traveling", "bananas", "texas", "succulents", "sunshine", "running", "paris", "sleep"];
-var topic = $('#topic').val();
 
+var topics = ["trees", "tigers", "traveling", "bananas", "texas", "succulents", "sunshine", "running", "paris", "sleep"]
 
     createButtons();
     //Building buttons to loop through array
@@ -9,7 +8,8 @@ var topic = $('#topic').val();
         for (var i = 0; i < topics.length; i++) {
             var button = $('<button>');
             button.attr('data-name', topics[i]);
-            button.attr('id', 'topic')
+            button.val(topics[i]);
+            button.addClass('topic')
             button.addClass('btn btn-light')
             button.text(topics[i]);
             $('#buttonDisplay').append(button);
@@ -21,7 +21,7 @@ var topic = $('#topic').val();
         event.preventDefault();
 
         var addedTopic = $('#inputTopic').val().trim();
-        console.log(addedTopic);
+        // console.log(addedTopic);
 
         topics.push(addedTopic);
         $('#inputTopic').val("");
@@ -30,22 +30,33 @@ var topic = $('#topic').val();
 
     //call to GIPHY api
 
-    function giphyCall () {
+    $('.topic').on('click', function() {
+        var topic = $(this).val();
         var apiKey = "HfzaLobXPlhuhwVrZxOCHvuJTyKDpo5m";
         var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + topic + "&limit=10&rating=G&lang=en";
+        
 
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
+            // console.log(response.data);
+            //Currently throwing an 'undefined' once .images is attached
+            var imageUrl = response.data.images.fixed_height_still.url;
+            console.log("This is what is getting passed: " + topic)
+            console.log(imageUrl);
 
             //create gifs for each that is returned, based on the search params
+            var gifCard = $("<div class='card'>");
+            var gifImage = $("<img>").attr("src", imageUrl);
+            gifCard.append(gifImage);
+            $('#gifDisplay').append(gifCard);
 
         })
         console.log(queryUrl);
-    }
+    })
     
-    $(document).on('click', '#topic', giphyCall);
+    
+    // $(document).on('click', '#topic', giphyCall);
     // $('.topic').on('click', giphyCall());
 
