1180d526d6024b755415a176b2e1a48c506457d0
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-07-09 20:07:15 -0400

    gif call

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,7 @@
 var topics = ["trees", "tigers", "traveling", "bananas", "texas", "succulents", "sunshine", "running", "paris", "sleep"];
+var topic = $('#topic').val();
 
-$(document).ready(function() {
+
     createButtons();
     //Building buttons to loop through array
     function createButtons () {
@@ -8,7 +9,7 @@ $(document).ready(function() {
         for (var i = 0; i < topics.length; i++) {
             var button = $('<button>');
             button.attr('data-name', topics[i]);
-            button.addClass('topic')
+            button.attr('id', 'topic')
             button.addClass('btn btn-light')
             button.text(topics[i]);
             $('#buttonDisplay').append(button);
@@ -28,4 +29,23 @@ $(document).ready(function() {
     })
 
     //call to GIPHY api
-})
+
+    function giphyCall () {
+        var apiKey = "HfzaLobXPlhuhwVrZxOCHvuJTyKDpo5m";
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + topic + "&limit=10&rating=G&lang=en";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+
+            //create gifs for each that is returned, based on the search params
+
+        })
+        console.log(queryUrl);
+    }
+    
+    $(document).on('click', '#topic', giphyCall);
+    // $('.topic').on('click', giphyCall());
+
